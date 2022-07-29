8cd2ed5c1e977a1aec4c51884bb8ceccf4d76e1b
Trevor Burnham <trevor.burnham@compass.com>
2022-07-29 10:39:39 -0400

    Add source-map-loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,5 +8,21 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+
+  webpackFinal: async (
+    /** @type { import('webpack').Configuration } */ config
+  ) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      enforce: 'pre',
+      use: [
+        {
+          loader: 'source-map-loader',
+        },
+      ],
+    });
+
+    return config;
+  }
 }
